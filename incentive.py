1e79e2db4ecbf040c7c495364d9c641e7d845e88
Atul Singh <atulpratapsingh05@gmail.com>
2022-11-10 21:07:10 +0530

    Initialize incentive python

--- incentive.py
+++ incentive.py
@@ -23,9 +23,8 @@ def unzip_and_analyze(filename):
     os.chdir(basepath)
     combined_df.to_excel('total_production.xlsx', index=False);
     combined_df.to_csv('total_production.csv', index=False, sep='|')
-    os.chdir(newpath)
-    
-    #Analyze individual shift files
+
+    #Get date shift section
     excel_references = defaultdict(dict)
     for file in excel_files:
         section_name = file.split('.')[0].split('_')[3]
@@ -34,36 +33,36 @@ def unzip_and_analyze(filename):
     
     excel_references = dict(excel_references)
     
-    print('\n---------------------- INDIVIDUAL SHIFT & SECTION ANALYSIS - COAL ----------------------\n')
-    for section,obj in excel_references.items():
-        for shift,df in obj.items():
-            print('Coal Production ({} {}): {} Trips {} Tonnes.'
-                  .format(section, shift, df[df['Coal dumped'] >= 0]['Dumper_Number_of_Trips'].sum(), df['Coal dumped'].sum()))
-
-    print('\n---------------------- INDIVIDUAL SHIFT & SECTION ANALYSIS - OB ----------------------\n')
-    for section,obj in excel_references.items():
-        for shift,df in obj.items():
-            print('OB Production ({} {}): {} Trips {} Cum.'
-                  .format(section, shift, df[df['OB Dumped'] >= 0]['Dumper_Number_of_Trips'].sum(), df['OB Dumped'].sum()))
-
-    #Analyze total production file
-    os.chdir(basepath)
-    the_file = pd.read_excel('total_production.xlsx')
-
-    print('\n---------------------- ANALYSIS OF {} ----------------------'.format('total_production.xlsx'))
-    print("\nTotal {} rows.\n".format(len(the_file.index)))
-    print('Total Trips: {}.\n'.format(the_file['Dumper_Number_of_Trips'].sum()))
-    print('Total Coal Production: {} Tonnes.\n'.format(the_file['Coal dumped'].sum()))
-    print('Total OB Production: {} Cum.\n'.format(the_file['OB Dumped'].sum()))
-    print('\nData rows per Process-Order \n')
-    print(pd.pivot_table(the_file, values='Coal dumped', columns=['Process_Order'], index='shift', aggfunc=np.count_nonzero))
-    print('\nProcess-Order-wise Total Production --- COAL\n')
-    print(pd.pivot_table(the_file, values='Coal dumped', columns=['Process_Order'], index='shift', aggfunc=np.sum).fillna(0))
-    print('\nOB - Process-Order-wise Total Production --- OB\n')
-    print(pd.pivot_table(the_file, values='OB Dumped', columns=['Process_Order'], index='shift', aggfunc=np.sum).fillna(0))
-    
-    print('\nProcess-Order-wise Overall Totals\n')
-    print(the_file[['Process_Order', 'Coal dumped', 'OB Dumped']].groupby('Process_Order').sum())
+    # print('\n---------------------- INDIVIDUAL SHIFT & SECTION ANALYSIS - COAL ----------------------\n')
+    # for section,obj in excel_references.items():
+    #     for shift,df in obj.items():
+    #         print('Coal Production ({} {}): {} Trips {} Tonnes.'
+    #               .format(section, shift, df[df['Coal dumped'] >= 0]['Dumper_Number_of_Trips'].sum(), df['Coal dumped'].sum()))
+    #
+    # print('\n---------------------- INDIVIDUAL SHIFT & SECTION ANALYSIS - OB ----------------------\n')
+    # for section,obj in excel_references.items():
+    #     for shift,df in obj.items():
+    #         print('OB Production ({} {}): {} Trips {} Cum.'
+    #               .format(section, shift, df[df['OB Dumped'] >= 0]['Dumper_Number_of_Trips'].sum(), df['OB Dumped'].sum()))
+    #
+    # #Analyze total production file
+    # os.chdir(basepath)
+    # the_file = pd.read_excel('total_production.xlsx')
+    #
+    # print('\n---------------------- ANALYSIS OF {} ----------------------'.format('total_production.xlsx'))
+    # print("\nTotal {} rows.\n".format(len(the_file.index)))
+    # print('Total Trips: {}.\n'.format(the_file['Dumper_Number_of_Trips'].sum()))
+    # print('Total Coal Production: {} Tonnes.\n'.format(the_file['Coal dumped'].sum()))
+    # print('Total OB Production: {} Cum.\n'.format(the_file['OB Dumped'].sum()))
+    # print('\nData rows per Process-Order \n')
+    # print(pd.pivot_table(the_file, values='Coal dumped', columns=['Process_Order'], index='shift', aggfunc=np.count_nonzero))
+    # print('\nProcess-Order-wise Total Production --- COAL\n')
+    # print(pd.pivot_table(the_file, values='Coal dumped', columns=['Process_Order'], index='shift', aggfunc=np.sum).fillna(0))
+    # print('\nOB - Process-Order-wise Total Production --- OB\n')
+    # print(pd.pivot_table(the_file, values='OB Dumped', columns=['Process_Order'], index='shift', aggfunc=np.sum).fillna(0))
+    #
+    # print('\nProcess-Order-wise Overall Totals\n')
+    # print(the_file[['Process_Order', 'Coal dumped', 'OB Dumped']].groupby('Process_Order').sum())
     
     #print('\n-----------------------------OTHER TOTALS---------------------------')
     #print('\nSHIFTWISE PRODUCTION TOTALS:\n')
