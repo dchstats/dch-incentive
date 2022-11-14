c065ac867b7a31e71c1b84fcabfc5152972a0f5e
Atul Singh <atulpratapsingh05@gmail.com>
2022-11-14 17:49:17 +0530

    Add boilerplate file upload from https://www.codingnepalweb.com/drag-drop-file-upload-feature-javascript/

--- index.js
+++ index.js
@@ -1,42 +1,58 @@
-const { app, BrowserWindow } = require('electron')
+//selecting all required elements
+const dropArea = document.querySelector(".drag-area"),
+dragText = dropArea.querySelector("header"),
+button = dropArea.querySelector("button"),
+input = dropArea.querySelector("input");
+let file; //this is a global variable and we'll use it inside multiple functions
 
-function createWindow () {
-// Create the browser window.
-const win = new BrowserWindow({
-	width: 800,
-	height: 600,
-	webPreferences: {
-	nodeIntegration: true
-	}
-})
-
-// Load the index.html of the app.
-win.loadFile('src/index.html')
-
-// Open the DevTools.
-win.webContents.openDevTools()
+button.onclick = ()=>{
+  input.click(); //if user click on the button then the input also clicked
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-// This method is equivalent to 'app.on('ready', function())'
-app.whenReady().then(createWindow)
+input.addEventListener("change", function(){
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = this.files[0];
+  dropArea.classList.add("active");
+  showFile(); //calling function
+});
 
-// Quit when all windows are closed.
-app.on('window-all-closed', () => {
-// On macOS it is common for applications and their menu bar
-// to stay active until the user quits explicitly with Cmd + Q
-if (process.platform !== 'darwin') {
-	app.quit()
-}
-})
 
-app.on('activate', () => {
-	// On macOS it's common to re-create a window in the
-	// app when the dock icon is clicked and there are no
-	// other windows open.
-if (BrowserWindow.getAllWindows().length === 0) {
-	createWindow()
+//If user Drag File Over DropArea
+dropArea.addEventListener("dragover", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  dropArea.classList.add("active");
+  dragText.textContent = "Release to Upload File";
+});
+
+//If user leave dragged File from DropArea
+dropArea.addEventListener("dragleave", ()=>{
+  dropArea.classList.remove("active");
+  dragText.textContent = "Drag & Drop to Upload File";
+});
+
+//If user drop File on DropArea
+dropArea.addEventListener("drop", (event)=>{
+  event.preventDefault(); //preventing from default behaviour
+  //getting user select file and [0] this means if user select multiple files then we'll select only the first one
+  file = event.dataTransfer.files[0];
+  showFile(); //calling function
+});
+
+function showFile(){
+  let fileType = file.type; //getting selected file type
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+  if(validExtensions.includes(fileType)){ //if user selected file is an image file
+    let fileReader = new FileReader(); //creating new FileReader object
+    fileReader.onload = ()=>{
+      let fileURL = fileReader.result; //passing user file source in fileURL variable
+        // UNCOMMENT THIS BELOW LINE. I GOT AN ERROR WHILE UPLOADING THIS POST SO I COMMENTED IT
+      // let imgTag = `<img src="${fileURL}" alt="image">`; //creating an img tag and passing user selected file source inside src attribute
+      dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
+    }
+    fileReader.readAsDataURL(file);
+  }else{
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
 }
-})
